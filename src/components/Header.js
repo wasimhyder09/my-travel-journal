70ac85d15cd34861466bd76a14cb732dc6b9be82
Wasim Hyder <wasim@creativefaze.com>
2022-08-26 17:30:09 +0500

    Created header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+
+export default function Header() {
+  return(
+    <div className="header">
+      <img src="/images/logo.png" className="logo" alt="logo"></img>
+      <h3 className="name">my travel journal.</h3>
+    </div>
+  )
+}
